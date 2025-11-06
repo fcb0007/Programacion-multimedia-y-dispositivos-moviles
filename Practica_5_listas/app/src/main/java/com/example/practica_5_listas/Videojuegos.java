99679bf452fbc091121a18f7950b3cdec8e79032
Francisco Cerezo <fcb0007@alu.medac.es>
2025-11-06 20:31:55 +0100

    LLego hasta el punto 2.4 del ejercicio de las listas

---
+++ Practica_5_listas/app/src/main/java/com/example/practica_5_listas/Videojuegos.java
@@ -0,0 +1,10 @@
+package com.example.practica_5_listas;
+
+public class Videojuegos {
+    private String nombre;
+    private String genero;
+    private int año;
+    private String imagen;
+
+
+}
